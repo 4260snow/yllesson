45a94c45debec3bec33667d1471b09299529da10
4260snow <irbis314@yandex.ru>
2021-03-26 18:37:37 +0300

    изменён main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_ngrok import run_with_ngrok
 
@@ -8,9 +7,8 @@ run_with_ngrok(app)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask 2"
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
